2c272d719158e3cd4fc447d8b82c0bcc5ebf62ac
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-16 13:58:23 +0100

    Add files via upload

---
+++ projet pendu.py
@@ -0,0 +1,7 @@
+# Créé par Samson.webervillaumi, le 16/11/2023 en Python 3.7
+
+def progression(lettre):
+    for nombre in range(len(progression)):
+        if progression[nombre]==lettre:
+            progression[nomnbre] = lettre
+    return progression
