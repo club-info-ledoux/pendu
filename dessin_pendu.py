15b3eb137c7add6ff69c6e1862155f8d00774c5f
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-23 13:20:54 +0100

    Add files via upload

--- dessin_pendu.py
+++ dessin_pendu.py
@@ -1,2 +1,47 @@
 # Créé par elias.kuhm, le 16/11/2023 en Python 3.7
 
+#creation d'un pendu en équipe
+#affichage dessin du pendu
+
+#idée dessin
+'''
+  _____
+  |    |
+  |    ☠
+__|__
+
+'''
+def dessin_pendu(erreur):
+    if erreur == 1:
+        print("_____")
+    elif erreur == 2:
+        print("""
+  |
+  |
+__|__""")
+    elif erreur == 3:
+        print("""
+  ______
+  |
+  |
+__|__""")
+    elif erreur == 4:
+        print("""
+  ______
+  |    |
+  |
+__|__""")
+    elif erreur == 5:
+        print("""
+  ______
+  |    |
+  |    ☠
+__|__""")
+    else:
+        pass
+
+'''
+test
+'''
+for i in range(1,5,1):
+    dessin_pendu(i)
