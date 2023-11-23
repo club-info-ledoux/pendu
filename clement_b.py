e7bea259180f20b31166c0dfce5c19b1fdbc44f9
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-23 13:17:39 +0100

    Add files via upload

---
+++ clement_b.py
@@ -0,0 +1,16 @@
+# Créé par clement.brager, le 16/11/2023 en Python 3.7
+#update tirert
+def initialisation_progression(mot_a_trouver):
+    progression="_ "
+    for i in range(len(mot_a_trouver)-1):
+        progression+="_ "
+    return progression
+
+def affichage_tiret(progression):
+    print("progression du mot: ")
+    print(progression)
+
+
+
+
+
