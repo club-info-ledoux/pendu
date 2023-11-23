1d656b984637483b4563933a9575dad983f96a3f
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-23 13:47:13 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -25,3 +25,18 @@ def demander_choix():
     choix=choix.upper()                              #upper() mets toutes le lettres en majuscules tout mettre en majuscules
     progression=choix
     return progression
+
+
+#update tirets
+def initialisation_progression(mot_a_trouver):
+    progression = "_"
+    for i in range(len(mot_a_trouver)-1):
+        progression += "_"
+    return progression
+
+def affichage_tiret(progression):
+    affichage = ""
+    for n in range(len(progression)):
+        affichage += progression[n] + " "
+    print("progression du mot: ")
+    print(affichage)
