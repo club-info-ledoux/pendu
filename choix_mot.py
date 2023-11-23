87dc6316a73ee0a8c0de6c08e0c738ee0fd083da
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-23 13:26:34 +0100

    Add files via upload

--- choix_mot.py
+++ choix_mot.py
@@ -1,14 +1,19 @@
 import random
 
 def choisir_mot():
-    special_char = ["éèëê", "ùüû", "ôö", "îï", "àäâ"]
-    fichier_dico = open("dico.txt", 'r')
+    #choisir un mot aléatoir dans le fichier dico.txt
+    fichier_dico = open("dico.txt", 'r', encoding='utf-8')
     liste_dico = fichier_dico.readlines()
     mot = random.choice(liste_dico)
     mot = mot.replace('\n', '')
+
+    #remplacement des accents
+    special_char = ["eéèëê", "uùüû", "oôö", "iîï", "aàäâ"]
     for letter in special_char:
-        pass
+        for accent in letter[1:]:
+            if accent in mot:
+                mot = mot.replace(accent, letter[0])
+
+    #capitalisation du mot
     mot = mot.upper()
     return mot
-
-print(choisir_mot())
