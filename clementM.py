1d2d945c19372c941230960b9564db8ce8540ea5
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-23 13:39:36 +0100

    demander_lettre.py

--- clementM.py
+++ clementM.py
@@ -3,13 +3,11 @@
 #importation des modules
 
 
-def lechoix():
-
+def demander_choix():
     choix=input("Quelle lettre voulez vous essayer ? ")
-    while len(choix)!=1 or choix.isalpha()==False:       #isalpha() envoie True si ce n'est que des lettres
+    while len(choix)!=1 or not choix.isalpha():       #isalpha() envoie True si ce n'est que des lettres
         choix=input("N'entrez qu'une lettre")
-    if choix.isupper()==False:                           #isupper() envoie True si les lettres sont toutes en majuscules
-        choix=choix.upper()                              #upper() mets toutes le lettres en majuscules tout mettre en majuscules
+    choix=choix.upper()                              #upper() mets toutes le lettres en majuscules tout mettre en majuscules
     progression=choix
     return progression
 
