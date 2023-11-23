1065614c56e4b3b25fd9dea7a7cb2d2ce189ca69
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-23 13:56:51 +0100

    Update projet pendu.py

--- projet pendu.py
+++ projet pendu.py
@@ -1,5 +1,5 @@
 # Créé par Samson.webervillaumi, le 16/11/2023 en Python 3.7
-
+# supprimer ce fichier je l'ai déjà renommer/ajouter en "progression_mot" dans github
 
 def progression(lettre,mot,progression):
     positions_lettre = verification(mot,lettre)
