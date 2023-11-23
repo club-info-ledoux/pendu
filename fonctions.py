0a8d14274c79a808ab4567fdc72ae28ef21e83f4
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-23 13:45:01 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -1,1 +1,27 @@
+import random
 
+def choisir_mot():
+    #choisir un mot aléatoir dans le fichier dico.txt
+    fichier_dico = open("dico.txt", 'r', encoding='utf-8')
+    liste_dico = fichier_dico.readlines()
+    mot = random.choice(liste_dico)
+    mot = mot.replace('\n', '')
+
+    #remplacement des accents
+    special_char = ["eéèëê", "uùüû", "oôö", "iîï", "aàäâ"]
+    for letter in special_char:
+        for accent in letter[1:]:
+            if accent in mot:
+                mot = mot.replace(accent, letter[0])
+
+    #capitalisation du mot
+    mot = mot.upper()
+    return mot
+
+def demander_choix():
+    choix=input("Quelle lettre voulez vous essayer ? ")
+    while len(choix)!=1 or not choix.isalpha():       #isalpha() envoie True si ce n'est que des lettres
+        choix=input("N'entrez qu'une lettre")
+    choix=choix.upper()                              #upper() mets toutes le lettres en majuscules tout mettre en majuscules
+    progression=choix
+    return progression
