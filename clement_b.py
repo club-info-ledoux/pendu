7b2a8208fdec52b239e01064ea3e8a603302ea5b
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-23 13:47:39 +0100

    Update clement_b.py

--- clement_b.py
+++ clement_b.py
@@ -1,21 +1,13 @@
-# Créé par clement.brager, le 16/11/2023 en Python 3.7
-#update tirert
+#update tirets
 def initialisation_progression(mot_a_trouver):
-    progression="_"
+    progression = "_"
     for i in range(len(mot_a_trouver)-1):
-        progression+="_"
+        progression += "_"
     return progression
 
 def affichage_tiret(progression):
-    affichage=""
+    affichage = ""
     for n in range(len(progression)):
-        affichage+= progression[n]+" "
+        affichage += progression[n] + " "
     print("progression du mot: ")
     print(affichage)
-
-
-
-
-
-
-
