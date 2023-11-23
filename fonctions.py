caa00be31e9f7b7a7d33b29d43fb66a2365878bb
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-23 13:53:45 +0100

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -40,3 +40,42 @@ def affichage_tiret(progression):
         affichage += progression[n] + " "
     print("progression du mot: ")
     print(affichage)
+
+
+#affichage dessin du pendu
+#idée dessin
+'''
+  _____
+  |    |
+  |    ☠
+__|__
+
+'''
+def dessin_pendu(erreur):
+    if erreur == 1:
+        print("_____")
+    elif erreur == 2:
+        print("""
+  |
+  |
+__|__""")
+    elif erreur == 3:
+        print("""
+  ______
+  |
+  |
+__|__""")
+    elif erreur == 4:
+        print("""
+  ______
+  |    |
+  |
+__|__""")
+    elif erreur == 5:
+        print("""
+  ______
+  |    |
+  |    ☠
+__|__""")
+    else:
+        pass
