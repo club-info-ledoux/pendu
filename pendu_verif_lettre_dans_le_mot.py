7051b15557da63eb5b1b82040113d494ee5a7ab6
club-info-ledoux <151030950+club-info-ledoux@users.noreply.github.com>
2023-11-23 13:48:42 +0100

    Update pendu_verif_lettre_dans_le_mot.py

--- pendu_verif_lettre_dans_le_mot.py
+++ pendu_verif_lettre_dans_le_mot.py
@@ -1,6 +1,10 @@
-def verification(lettre_choisie):
+def verification(mot_a_trouver, lettre_choisie):
+    global erreur
+    lettre_bonne = []
     for i in range(len(mot_a_trouver)):
         if lettre_choisie == mot_a_trouver[i]:
-            progression(lettr_choisie)
-        else:
-            erreur += 1
+            lettre_bonne.append(i)
+    if lettre_bonne == []:
+        erreur += 1
+        return erreur
+    return lettre_bonne
